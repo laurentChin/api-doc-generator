7f1f7bc6911746dfed97c2b9edbc1640353c4d5b
Laurent Janet <laurent.janet@backelite.com>
2015-11-13 15:23:36 +0100

    create js gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,16 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    babel = require('gulp-babel'),
+    eslint = require('gulp-eslint'),
+    sass = require('gulp-sass'),
+    plumber = require('gulp-plumber'),
+    inject = require('gulp-inject'),
+    runSequence = require('run-sequence');
 
-gulp.task('default', function(){});
+gulp.task('js', function(){
+    return gulp.src('./src/js/app.js')
+        .pipe(plumber())
+        .pipe(babel())
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(gulp.dest('./build/js/app.js'));
+});
